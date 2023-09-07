fce5d9d6d742f748c15e85a8d79d346a07c9586c
Anda Brown <103917160+AndaBrown@users.noreply.github.com>
2023-09-07 23:47:16 +0800

    更新 config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -96,7 +96,7 @@ export default defineUserConfig({
     plugins: [
       docsearchPlugin({
         appId: 'FLBJDNSJ4H',
-        apiKey: '4f79f1c7c76126cc9cbf510cb3f6661d',
+        apiKey: 'f0fde104fdda0ec7d000a2b7e2234200',
         indexName: 'tgwiki',
         locales: {
           '/': {
