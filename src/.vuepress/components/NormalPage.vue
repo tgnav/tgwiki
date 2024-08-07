147b9c35fd4ac4cb14911e9336dc251c86e5040e
Anda Brown <103917160+AndaBrown@users.noreply.github.com>
2024-08-07 16:46:29 +0800

    更新

--- src/.vuepress/components/NormalPage.vue
+++ src/.vuepress/components/NormalPage.vue
@@ -4,7 +4,7 @@ import NormalPage from "vuepress-theme-hope/components/NormalPage.js";
 <template>
   <NormalPage>
     <template #tocBefore>
-      <a href="https://t.me/tgnav0bot" target="_blank"><div style="text-align: center; max-width: 300px;"><img src="https://s2.loli.net/2024/06/27/gtuVsDoMT1v6RdK.jpg" alt="广告位招商" width="100%"></div></a>
+      <a href="https://t.me/tgnav0bot" target="_blank"><div style="text-align: center; max-width: 240px;"><img src="https://s2.loli.net/2024/06/27/gtuVsDoMT1v6RdK.jpg" alt="广告位招商" width="100%"></div></a>
     </template>
   </NormalPage>
 </template>
