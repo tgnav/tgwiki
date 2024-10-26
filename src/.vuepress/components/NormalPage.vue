b179d83d53f14dca8acbb849bd9c5ab2ff20108b
Anda Brown <103917160+AndaBrown@users.noreply.github.com>
2024-10-26 19:41:33 +0800

    更新

--- src/.vuepress/components/NormalPage.vue
+++ src/.vuepress/components/NormalPage.vue
@@ -4,7 +4,7 @@ import NormalPage from "vuepress-theme-hope/components/NormalPage.js";
 <template>
   <NormalPage>
     <template #tocBefore>
-      <a href="/link/?url=www.smsrecv.com" target="_blank"><div style="text-align: center; max-width: 240px;"><img src="https://s2.loli.net/2024/09/17/B5lsEcUKZIO4nSj.png" alt="SmsRecv全球接码" width="100%"></div></a>
+      <a href="https://t.me/tgnav0bot" target="_blank"><div style="text-align: center; max-width: 240px;"><img src="https://s2.loli.net/2024/06/27/gtuVsDoMT1v6RdK.jpg" alt="广告位招商" width="100%"></div></a>
     </template>
   </NormalPage>
 </template>
