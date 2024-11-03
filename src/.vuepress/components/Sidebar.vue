fadc2295aef4feb432e99e4158a63304ecb1037a
Anda Brown <103917160+AndaBrown@users.noreply.github.com>
2024-11-03 23:21:07 +0800

    更新

--- src/.vuepress/components/Sidebar.vue
+++ src/.vuepress/components/Sidebar.vue
@@ -4,7 +4,7 @@ import Sidebar from "vuepress-theme-hope/modules/sidebar/components/Sidebar.js";
 <template>
   <Sidebar>
     <template #top>
-      <a href="https://tgnav.github.io/link/?url=87998.wang" target="_blank"><div style="text-align: center; max-width: 300px;"><img src="https://s2.loli.net/2024/09/08/Vtrx3UDpIG1kvR2.jpg" alt="红杏IEPL专线机场" width="100%"></div></a>
+      <a href="https://tgnav.github.io/link/?url=t.me/+tkbddlpEZIk2MDQ1" target="_blank"><div style="text-align: center; max-width: 300px;"><img src="https://s2.loli.net/2024/11/03/2RZhnLVkz5dx8XG.png" alt="景福宫全国外围" width="100%"></div></a>
     </template>
   </Sidebar>
 </template>
