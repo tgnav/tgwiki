e4d131c7c2a4560215cbb215e868864de761e029
Anda Brown <103917160+AndaBrown@users.noreply.github.com>
2025-02-18 01:21:15 +0800

    更新

--- src/.vuepress/components/Sidebar.vue
+++ src/.vuepress/components/Sidebar.vue
@@ -4,7 +4,7 @@ import Sidebar from "vuepress-theme-hope/modules/sidebar/components/Sidebar.js";
 <template>
   <Sidebar>
     <template #top>
-      <a href="https://t.me/tgnav0bot" target="_blank"><div style="text-align: center; max-width: 300px;"><img src="https://s2.loli.net/2024/06/27/gtuVsDoMT1v6RdK.jpg" alt="广告位招商" width="100%"></div></a>
+      <a href="https://tgnav.github.io/link/?url=reurl.cc/ZVRjog" target="_blank"><div style="text-align: center; max-width: 300px;"><img src="https://cdn.jsdelivr.net/gh/tgwiki/images/@ad/xsus.jpg" alt="xsus" width="100%"></div></a>
     </template>
   </Sidebar>
 </template>
